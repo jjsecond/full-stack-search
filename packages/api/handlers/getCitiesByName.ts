3f21e9ca5d74c9b3eddbb6e19bf6bf8d7a3b486c
jjsecond <jjvonbham@gmail.com>
2025-02-11 13:52:16 +0000

    FEAT(api): Adds endpoints for searching for collections with regex

--- packages/api/handlers/getCitiesByName.ts
+++ packages/api/handlers/getCitiesByName.ts
@@ -1,22 +1,31 @@
-import { MongoClient } from "mongodb";
 import { Request, Response } from 'express';
 import { COLLECTION_NAMES } from "lib/constants/collections";
+import { MongoClient } from 'mongodb';
 
-const getAllCities = async (req: Request, res: Response) =>{
+const getCitiesByName = async (req: Request, res: Response) =>{
     // TODO: Sort out type on url
       const mongoClient = new MongoClient(process.env.DATABASE_URL || '');
-      console.log('Connecting to MongoDB...');
+
+      // TODO: Check that it is valid
+      const { name } = req.params;
     
       try {
         await mongoClient.connect();
         console.log('Successfully connected to MongoDB!');
+
+        const cityName = new RegExp(name, 'i');
+
         const db = mongoClient.db()
         const collection = db.collection(COLLECTION_NAMES.CITIES);
-        res.send(await collection.find().toArray())
+
+        let result = await collection.find(
+            { name: {$regex: cityName }}).limit(10).toArray();
+
+        res.status(200).json(result)
       } finally {
         await mongoClient.close();
       }
 };
 
 
-export default getAllCities;
+export default getCitiesByName;
