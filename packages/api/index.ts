d9173d1da8e0d03f9e0d939e08f810692e362747
jjsecond <jjvonbham@gmail.com>
2025-02-11 11:44:49 +0000

    FEAT(API): adds express router and basic handlers

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,37 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
-import { MongoClient } from "mongodb";
+import { routerV1 }  from "routes/routesV1";
 
 dotenv.config();
-
 if (process.env.NODE_ENV !== 'production' && !process.env.DATABASE_URL) {
   await import('./db/startAndSeedMemoryDB');
 }
 
 const PORT = process.env.PORT || 3001;
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
 
-app.get('/hotels', async (req, res) => {
-  const mongoClient = new MongoClient(DATABASE_URL);
-  console.log('Connecting to MongoDB...');
+// Adds a simple versioning for routes to enable later api updates in different versions and so that clients can be slowly transitioned to new versions should the need arise
+app.use(`/v1`, routerV1);
 
-  try {
-    await mongoClient.connect();
-    console.log('Successfully connected to MongoDB!');
-    const db = mongoClient.db()
-    const collection = db.collection('hotels');
-    res.send(await collection.find().toArray())
-  } finally {
-    await mongoClient.close();
-  }
-})
 
 app.listen(PORT, () => {
   console.log(`API Server Started at ${PORT}`)
---
+++ packages/api/lib/constants/collections.ts
@@ -0,0 +1,7 @@
+// The names of the current collections in the MongoDB
+export const COLLECTION_NAMES = {
+    CITIES: 'cities',
+    COUNTRIES: 'countries',
+    HOTELS: 'hotels',
+  };
+  
